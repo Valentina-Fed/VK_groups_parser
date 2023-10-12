858fd3a05b54192e3a97ca9dbf9d18eefbc955df
Valentina-Fed <76586010+Valentina-Fed@users.noreply.github.com>
2023-10-12 13:26:06 +0200

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -10,6 +10,7 @@ import nltk
 import re
 import os
 import pandas as pd
+import itertools
 import matplotlib.pyplot as plt
 from spacy.lang.ru import Russian
 nltk.download('punkt') # Loads a module required for tokenization
@@ -18,15 +19,8 @@ nlp = Russian()
 morph = pymorphy2.MorphAnalyzer()
 
 def lemmatize(text):
-    words = []
-    for tx in text:
-      if type(tx) == str:
-        words.extend(tx.split())
-    res = list()
-    for word in words:
-        p = morph.parse(word)[0]
-        res.append(p.normal_form)
-    return res
+    words = list(itertools.chain(*[tx.split() for tx if type(tx) == str in text]))
+    return [morph.parse(word)[0].normal_form for word in words]
 
 stopw = ['её', 'd0', 'ли', 'том,',':', 'быть', 'тоже', 'около', 'чтоб', 'н', 'уж', '3gp', '8b', 'bd', 'b2', '3d', 'д', 'т', 'п', 'тем', 'между', 'пока', 'поэтому', 'про', 'среди', 'лишь', 'некоторый', 'и,', 'тогда', 'перед', 'ru', 'bb', 'd1', 'над','она','.', 'до','или', 'только', 'то', 'так', 'такой', 'ещё', 'тот', 'кто', 'же', 'самый', 'есть', 'уже', 'при', 'очень', 'где', 'когда', 'другой', 'также', 'более', 'чтобы', 'если', 'себя', 'даже', 'да', 'бы', 'под', 'каждый', 'после', 'https://vk.com/mp3/audio_api_unavailable.mp3', 'без', 'несколько', ',', 'г.', 'в.', 'вот', 'там', 'ни', 'того,', 'какой', 'через', 'нет', 'много', 'кроме', 'раз', 'чем', 'почти', 'всего', 'сам', '::', 'в','и','на','с', 'не', 'по', 'весь', 'что', 'из', 'это', '–', 'а', 'они', 'о','к', 'как', 'он', 'для', 'один', 'который', 'от', 'у', 'свой', 'за', '—','этот','но']
 STOPWORDS.update(stopw)
@@ -51,14 +45,13 @@ def vocToStat(text):
 def create_wordcloud(path):
     for file in os.listdir(path):
       if file.endswith('wall_text.csv'):
-        df = pd.read_csv(f'{file}', sep='\t').to_dict()
-        text = df['text'].values()
-        text_lem=(' ').join(lemmatize(list(text)))
+        df = pd.read_csv(f'{file}', sep='\t')
+        text_lem = ' '.join(lemmatize(df['text']))
         phr = f'Словарь из 500 частотных слов из группы деревни {file}'
         dict_stat = vocToStat(text_lem)
         file2 = file.replace('_wall_text.csv', '')
         with open(f'{file2}.txt', 'w') as f:
-          f.write(phr + '\n' + dict_stat)
+          f.write(f'{phr}'\n'{dict_stat}')
         print(f'Printing wordcloud of the group {file2}')
         plot_cloud(text_lem, file)
 
